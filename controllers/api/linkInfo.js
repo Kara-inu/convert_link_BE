29c5d7b6bbaa941ceca7b5b352a4a4dea1bd9a83
Jonus <>
2023-05-11 22:34:51 +0700

    clear

--- controllers/api/linkInfo.js
+++ controllers/api/linkInfo.js
@@ -10,10 +10,8 @@ router
         try {
             let query = {short : req.params.key}
             const data = await LinlInfo.find(query);
-            console.log("get short", data)
             return res.status(200).send({ status: "success", data: data });
         } catch (error) {
-            console.log("err", error);
             return res.status(500).send({ status: "error", message: error.message });
         }
     })
@@ -21,29 +19,22 @@ router
         try {
             let query = req.query ? req.query : {};
             const data = await LinlInfo.find(query);
-            console.log("data", data)
             return res.status(200).send({ status: "success", data: data });
         } catch (error) {
-            console.log("err", error);
             return res.status(500).send({ status: "error", message: error.message });
         }
     })
     .post("/", async (req, res, next) => {
         try {
-            console.log('post link')
-            console.log("req.body", req.body)
             let datacreate = { ...req.body };
-            console.log('post datacreate', datacreate)
             const itemCreated = await LinlInfo.create(datacreate);
             return res.status(201).send({ status: "success", data: itemCreated })
         } catch (error) {
-            console.log("err", error);
             return res.status(500).send({ status: "error", message: error.message });
         }
     })
     .delete("/:id", async (req, res, next) => {
         let id = req.params.id
-        console.log("delete : ",id)
         const itemDeleted = await LinlInfo.deleteOne({ _id: id });
         return res.json({ data: itemDeleted });
     });
